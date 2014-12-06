c35ee68275291fc5db26ced80e9ad0e73e74c8b9
chutetearer <skipper2005@gmail.com>
2014-12-06 05:16:01 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do - A new one on 14/12/02
+## This is part of assignment 2 in the R
+## programming course. More details needed.
 
 ## Write a short comment describing this function
-
+## !!! Not yet clear what this function should do !!!
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve takes a matrix as an argument and
+## checks if its inverse is cached. If yes, it returns
+## the cached inverse matrix, otherwise it computes the
+## inverse, saves it in the cache and returns the inversed
+## matrix.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
